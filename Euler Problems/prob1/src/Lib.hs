2c461cf360aef81b299dae42cc1b7ed21278f1e0
Connie Chi <conniejchi@berkeley.edu>
2019-03-12 11:55:24 -0700

    figuring out print

--- Euler Problems/prob1/src/Lib.hs
+++ Euler Problems/prob1/src/Lib.hs
@@ -2,9 +2,17 @@ module Lib
     ( someFunc
     ) where
 
-multOfFiveOrThree :: Integer -> Integer
-multOfFiveOrThree x = x + 1
-modo :: Integer
-modo = 3 `mod` 2
-someFunc :: IO ()
-someFunc = print modo
+-- multOfFiveOrThree :: Integer -> Integer
+-- multOfFiveOrThree x = x + 1
+modoOfFive :: Integer -> Integer
+modoOfFive x = x `mod` 5
+modoOfThree :: Integer -> Integer
+modoOfThree x = x `mod` 3
+decrement:: Integer -> Integer
+decrement x = x - 1
+-- someFunc :: Integer -> IO()
+-- someFunc x = print x
+final:: Integer -> Integer
+final x = if (modoOfFive x) == 0 && (modoOfThree x) == 0
+    then print x
+    else print 1
