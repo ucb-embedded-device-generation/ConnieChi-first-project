2ee8442488117eb3f4a808dff2b273d0abae7191
Connie Chi <conniejchi@berkeley.edu>
2019-03-12 10:31:53 -0700

    something mkay?

--- my-project/src/Lib.hs
+++ my-project/src/Lib.hs
@@ -2,5 +2,7 @@ module Lib
     ( someFunc
     ) where
 
+message :: Bool
+message = (\x -> True)False
 someFunc :: IO ()
-someFunc = putStrLn "something other than \"Hello World \""
+someFunc = print message
