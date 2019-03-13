1704a42fe25a05ab69b28714177ade7a4fc35acf
Connie Chi <conniejchi@berkeley.edu>
2019-03-12 19:58:43 -0700

    finished part2

--- Euler Problems/prob2/app/Main.hs
+++ Euler Problems/prob2/app/Main.hs
@@ -2,5 +2,28 @@ module Main where
 
 import Lib
 
+
+-- fib :: Integer -> Integer
+-- fib 0 = 0
+-- fib 1 = 1
+-- fib x = fib(x - 1) + fib(x - 2)
+
+--lazy fibs, dunno how this works
+fib = 1 : 2 : zipWith (+) fib (tail fib)
+--Difference between integer and int?
+ifNotEven :: Int -> Int
+ifNotEven x = x `mod` 2
+
+evenSum :: Int -> Int
+-- evenSum 0 = 0
+evenSum x =
+  if (fib !! x) > 10
+    then 0
+  else if (ifNotEven (fib !! x)) == 0
+      then (fib !! x) + evenSum (x + 1)
+  else
+      evenSum (x + 1)
+
 main :: IO ()
-main = someFunc
+-- four million yayaya
+main = print (evenSum 0)
--- Euler Problems/prob2/src/Lib.hs
+++ Euler Problems/prob2/src/Lib.hs
@@ -2,5 +2,30 @@ module Lib
     ( someFunc
     ) where
 
+--
+-- -- fib :: Integer -> Integer
+-- -- fib 0 = 0
+-- -- fib 1 = 1
+-- -- fib x = fib(x - 1) + fib(x - 2)
+--
+-- --lazy fibs, dunno how this works
+-- fib = 1 : 2 : zipWith (+) fib (tail fib)
+-- --Difference between integer and int?
+-- ifNotEven :: Int -> Int
+-- ifNotEven x = x `mod` 2
+--
+-- evenSum :: Int -> Int
+-- -- evenSum 0 = 0
+-- evenSum x =
+--   if (fib !! x) > 4000000
+--     then 0
+--   else if (ifNotEven (fib !! x)) == 0
+--       then (fib !! x) + evenSum (x + 1)
+--   else
+--       evenSum (x + 1)
+--
+-- someFunc :: IO ()
+-- -- four million yayaya
+-- someFunc = print (evenSum 0)
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = print 1
