5894bf1bffb62448fec6eded80229ff3951bf4c9
Connie Chi <conniejchi@berkeley.edu>
2019-03-12 22:33:24 -0700

    stuck, :(

--- Euler Problems/prob4/app/Main.hs
+++ Euler Problems/prob4/app/Main.hs
@@ -2,5 +2,16 @@ module Main where
 
 import Lib
 
+--convert integer to list to reverse
+digs :: Integral x => x -> [x]
+digs 0 = []
+digs x = digs (x `div` 10) ++ [x `mod` 10]
+
+--need to add Eq because x can't be any type apparently?, == is type Eq
+isPalindrome :: Eq x => [x] -> Bool
+isPalindrome x = (reverse x == x)
+
+
+
 main :: IO ()
-main = someFunc
+main = print (isPalindrome (digs 9008))
