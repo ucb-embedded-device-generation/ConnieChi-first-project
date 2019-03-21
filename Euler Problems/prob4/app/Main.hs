df526caefa17059b9c39f11bf8ce3ab48d2b2b12
Connie Chi <conniejchi@berkeley.edu>
2019-03-20 22:15:32 -0700

    did it MYYYYY WAYYYYY

--- Euler Problems/prob4/app/Main.hs
+++ Euler Problems/prob4/app/Main.hs
@@ -1,15 +1,17 @@
 module Main where
 
 import Lib
---all three digit numbers
-threes :: [Int]
-threes = [x | y<-[100..999], z<-[100..999], let x = z * y, show x == reverse(show x)]
+--all three digit numbers with proper palindrome
+threes :: [Integer]
+-- threes = [x | y<-[100..999], z<-[100..999], let x = z * y, show x == reverse(show x)]
+threes = [x | y<-[100..999], z<-[100..999], let x = z * y, isPalindrome x == True]
 --convert integer to list to reverse
 digs :: Integral x => x -> [x]
 digs 0 = []
 digs x = digs (x `div` 10) ++ [x `mod` 10]
 
 --need to add Eq because x can't be any type apparently?, == is type Eq
+-- only for integer lists, dumb dumb me
 -- isPalindrome :: Eq x => [x] -> Bool
 -- isPalindrome :: Eq x => Bool
 -- isPalindrome x = (reverse x == x)
