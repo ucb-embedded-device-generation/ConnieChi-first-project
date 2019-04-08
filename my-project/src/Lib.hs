df0dc46caf7a7d13e4f8f8bdfb9155de88af92b9
Connie Chi <conniejchi@berkeley.edu>
2019-04-07 22:26:29 -0700

    tried to run seive but killed my CPU :(

--- my-project/src/Lib.hs
+++ my-project/src/Lib.hs
@@ -2,7 +2,27 @@ module Lib
     ( someFunc
     ) where
 
-message :: Bool
-message = (\x -> True)False
+-- message :: Bool
+-- message = (\x -> True)False
+
+array :: [Integer]
+array = [1, 2, 3]
+
 someFunc :: IO ()
-someFunc = print message
+someFunc = print (filter (<10) primes)
+
+-- joinStrings :: [String] -> String
+-- joinStrings []     = ""
+-- joinStrings (x:xs) = x ++ joinStrings xs
+
+-- | A list of all the prime numbers in order
+primes :: [Integer]
+primes = sieve [2..]
+
+-- | ????
+sieve :: [Integer] -> [Integer]
+sieve (x:xs) = x : sieve (filter (\y -> y `mod` x > 0) xs)
+
+
+-- lst :: [String]
+-- lst = ["toilets", "are", "fabulous"]
