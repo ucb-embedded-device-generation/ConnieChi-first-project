0fe152f1c2105825c0d557e6c99f35d79337ec42
Connie Chi <conniejchi@berkeley.edu>
2019-04-07 23:26:44 -0700

    still don't quite get it but found detailed explanation

--- Euler Problems/prob7/myproj/app/Main.hs
+++ Euler Problems/prob7/myproj/app/Main.hs
@@ -17,12 +17,19 @@ primes :: [Integer]
 -- divisible:: [Integer] -> [Integer] -> Bool
 -- divisible = True
 
+--we only run on prime numbers b/c evens are always gonna be non prime
+--we check if tail is null, this the thingamabob.
+--we start looking in primeFactors with 2 (???)
 primes = 2 : filter (null . tail . primeFactors) [3,5..]
 
 primeFactors n = factor n primes
 --recursive function here
   where
+    -- p is current item, ps is tail of list of prime factors :3
     factor n (p:ps)
+    --we know that there are no factors of n greater than the sqrt of n WOW MATH DOES THINGS so we just return n
         | p*p > n        = [n]
+        -- if n divisible by p, add p to prime factor list and check n divided by p as a new prime factor
         | n `mod` p == 0 = p : factor (n `div` p) (p:ps)
+        -- otherwise just set equal to another recursive call on tail of list
         | otherwise      =     factor n ps
