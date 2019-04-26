00e0d4f48e410ee6dd97ffa91f9b69016c98c47a
Connie Chi <conniejchi@berkeley.edu>
2019-04-26 12:41:20 -0700

    added package dependencies

--- parser/myproj/app/Main.hs
+++ parser/myproj/app/Main.hs
@@ -1,6 +1,10 @@
 module Main where
 
+import Text.ParserCombinators.Parsec --hiding (spaces)
 import Lib
 
 main :: IO ()
 main = someFunc
+
+symbol :: ParserChar
+symbol = oneOf "!$%&|*+-/:<=?>@^_~#"
